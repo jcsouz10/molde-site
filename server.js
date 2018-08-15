fb682232235ab102fab3c7e02225c41851cfa9d2
jcsouz10 <jeanrt1@hotmail.com>
2018-08-14 22:43:38 -0300

    feat: starts the backend, installs the express and axios.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const port = 9000;
+const app = express();
+const bodyParser = require('body-parser');
+
+let name;
+let product;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
+app.post('/api/post', (req, res) => {
+    name = req.body.name;
+    product = req.body.product;
+
+    if (name && product !== undefined)
+        res.send("Pedido realizado")  
+    else   
+        res.send("Pedido nao realizado")    
+})
+
+app.get("/", (req, res) => {
+    res.send(`O cliente foi ${name} e o pedido foi ${product}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server working on port ${port}`)
+})
+
